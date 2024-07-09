2fc6549c9406b82eadc53e206173ddea1bf99808
esenturDev <nurlanuuluesentur@gmail.com>
2024-07-09 21:35:47 +0600

    update

--- src/components/pages/home/Welcome.tsx
+++ src/components/pages/home/Welcome.tsx
@@ -1,12 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import scss from "./Welcome.module.scss";
 import { languageResultContext } from "../../../context/LanguageContext";
 import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
+import mePhoto from "../../../assets/photos/me photo.jpg";
+import { TechStack } from "../../../utils";
 type ContextType = {
 	language: string;
 	setLanguage: (value: string) => any;
 };
 export const Welcome = () => {
+	const [techStackName, setTechStackName] = useState<number | null>(null);
 	const { language, setLanguage } = useContext(languageResultContext);
 	console.log(language, "Welcome");
 	const ENMeText = "Hi, I m Esentur Ismailov";
@@ -30,10 +33,32 @@ export const Welcome = () => {
 									? "As an experienced Full-Stack developer, I am dedicated to turning ideas into innovative web applications. Explore my recent projects and articles showcasing my skills in both Back-End and Front-End development"
 									: "Как опытный Full-Stack разработчик, я стремлюсь превращать идеи в инновационные веб-приложения. Изучите мои недавние проекты и статьи, демонстрирующие мои навыки в области Back-End и Front-End-разработки."}
 							</p>
-							<div className={scss.icons_div}> 
-								<IconBrandGithub width={'45px'} height={'45px'}/>
-								<IconBrandLinkedin />
+							<div className={scss.icons_div}>
+								<a href="https://github.com/esenturDev">
+									<IconBrandGithub className={scss.icon} />
+								</a>
+								<a href="#">
+									<IconBrandLinkedin className={scss.icon} />
+								</a>
 							</div>
+						</div>
+						<img src={mePhoto} alt="" />
+					</div>
+					<div className={scss.Tech_Stack_div}>
+						<h3>{language === "EN" ? "Tech Stack |" : "Технический стек |"}</h3>
+						<div className={scss.cards}>
+							{TechStack.map((el, index) => (
+								<>
+									<div
+										onMouseEnter={() => setTechStackName(index + 1)}
+										onMouseLeave={() => setTechStackName(null)}
+                    className={scss.card}
+										key={index + 1}>
+										<img src={el.logo} alt={el.name} />
+									</div>
+									{techStackName === index + 1 && <p className={scss.TechStackText}>{el.name}</p>}
+								</>
+							))}
 						</div>
 					</div>
 				</div>
---
+++ src/utils/index.ts
@@ -0,0 +1,55 @@
+interface TechStackType {
+	logo: string;
+	name: string;
+}
+
+export const TechStack: TechStackType[] = [
+	{
+		logo: "https://v1.elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftype-script-icon.4ca749db.png&w=640&q=25",
+		name: "TypeScript",
+	},
+	{
+		logo: "https://v1.elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjava-script-icon.a4e1a162.png&w=640&q=25",
+		name: "JavaScript",
+	},
+	{
+		logo: "https://v1.elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Freact-icon.cd4a3653.png&w=640&q=25",
+		name: "React",
+	},
+	{
+		logo: "https://v1.elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fredux-icon.50f1c238.png&w=640&q=25",
+		name: "Redux Toolkit",
+	},
+	{
+		logo: "https://v1.elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnext-js-icon.93aa2cbd.png&w=640&q=25",
+		name: "Next.Js",
+	},
+	{
+		logo: "https://v1.elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fscss-icon.b18fa40c.png&w=640&q=25",
+		name: "SCSS",
+	},
+	{
+		logo: "https://v1.elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhtml-icon.3bdbae36.png&w=640&q=25",
+		name: "HTML",
+	},
+	{
+		logo: "https://v1.elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcss-icon.efcd8613.png&w=640&q=25",
+		name: "CSS",
+	},
+	{
+		logo: "https://v1.elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fvite-icon.a3686851.png&w=640&q=25",
+		name: "Vite",
+	},
+	{
+		logo: "https://v1.elcho.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fgit-icon.f6cf1da1.png&w=640&q=25",
+		name: "Git",
+	},
+	{
+		logo: "https://camo.githubusercontent.com/4ff735bdd177721778cfabd04f93466e54d2f334fb735fd0db10caffffe6988c/68747470733a2f2f736b696c6c69636f6e732e6465762f69636f6e733f693d6a6176617363726970742c747970657363726970742c66697265626173652c6e6578746a732c72656163742c626f6f7473747261702c6d75692c68746d6c2c6373732c7673636f64652c6769746875622c6669676d612c7461696c77696e642c6769742c76697465",
+		name: "Figma",
+	},
+	{
+		logo: "https://camo.githubusercontent.com/7789ebad547e48488bef791670ed2486a714ff41a230284952762c47b68a08de/68747470733a2f2f736b696c6c69636f6e732e6465762f69636f6e733f693d736373732c7374796c6564636f6d706f6e656e74732c72656475782c76657263656c",
+		name: "Vercel",
+	},
+];
